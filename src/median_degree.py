faaf66767be59524c54afbe3e51e307113b0429c
Ananya Mishra <am747@cornell.edu>
2016-07-05 23:42:02 -0700

    remove degree 0 nodes from calculation

--- src/median_degree.py
+++ src/median_degree.py
@@ -8,14 +8,20 @@ def convert_to_dt(dt_string):
 	return datetime.strptime(dt_string, '%Y-%m-%dT%H:%M:%SZ')
 
 def calc_median(G):
-	return np.median(nx.degree(G).values())
+	return np.median([deg for deg in G.degree().values() if deg > 0])
 
 class MedianGraph(object):
+
 	def __init__(self):
 		self.G = nx.Graph()
 		self.newest = None
-		self.degrees = []
 
+
+	'''
+	Returns true if a datetime lies in the 60s before the latest entry or anytime after it
+	Input: string
+	Returns: boolean
+	'''
 	def _within_window(self, created_time):
 		if not self.newest:
 			self.newest = convert_to_dt(created_time)
@@ -24,19 +30,33 @@ class MedianGraph(object):
 			return True
 		return False
 
+
+	'''
+	Removes edges outside of a 60s window of the most recently added edge.
+	'''
 	def _remove_old_edges(self):
 		created_times = nx.get_edge_attributes(self.G, "created_time")
 		for edge, created_time in created_times.iteritems():
 			if abs((self.newest - convert_to_dt(created_time)).seconds) > 60:
 				self.G.remove_edge(*edge)
 
+
+	'''
+	Add edges to graph if they are in the 60s window.
+	Input: JSON object
+	'''
 	def _add_valid_nodes(self, record):
 		if self._within_window(record['created_time']):
 			if record['target']:
 				self.G.add_edge(record['actor'], record['target'], created_time = record['created_time'])
 			self.newest = convert_to_dt(record['created_time'])
 			self._remove_old_edges()
+			# print self.G.size(), self.G.nodes()
 
+
+	'''
+	Begin the median calculation routine.
+	'''
 	def build_graph(self, infile, outfile):
 		with open(infile, 'r') as i, open(outfile, 'w') as o:
 			for line in i:
@@ -45,7 +65,6 @@ class MedianGraph(object):
 				median = calc_median(self.G)
 				o.write("%.2f" % (median) + '\n')
 				# print("Time: %s, %d Edges, Median: %.2f" % (self.newest, self.G.size(), median))
-				# print(np.mean(nx.degree(self.G).values()))
 				
 
 if __name__ == "__main__":
